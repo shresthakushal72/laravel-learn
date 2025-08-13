6c5c1d165c456509b780c98b891da164e354805c
Kushal Shrestha <shresthakushal72@gmail.com>
2025-08-13 19:33:34 +0200

    tailwind

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,11 +3,14 @@
 <head>
     <meta charset="UTF-8">
     <title>Homepage</title>
-    <link href="https://cdn.jsdelivr.net/npm/tailwindcss@3.4.1/dist/tailwind.min.css" rel="stylesheet">
+    <!-- Use Laravel's compiled CSS -->
+    @vite('resources/css/app.css')
 </head>
 <body class="bg-gray-100 flex items-center justify-center min-h-screen">
     <div class="bg-white p-8 rounded shadow text-center">
         <h1 class="text-2xl font-bold text-gray-800 mb-6">Welcome to Homepage Simple</h1>
+        <p class="text-2xl text-red-500 mb-7">Explore the features of your new homepage below.</p>
+        <p class="text-gray-500 mb-6">You can customize this page as needed.</p>
         <a href="/" class="inline-block px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600 transition">Back to Main</a>
     </div>
 </body>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,9 +3,11 @@
 <head>
     <meta charset="UTF-8">
     <title>Hello</title>
+        <!-- Use Laravel's compiled CSS -->
+    @vite('resources/css/app.css')
 </head>
 <body>
-    <h1>Hello</h1>
+    <h1 class="bg-red-200 font-extrabold">Hello</h1>
     <a href="{{ url('/home') }}">
         <button>Go to Home</button>
     </a>
